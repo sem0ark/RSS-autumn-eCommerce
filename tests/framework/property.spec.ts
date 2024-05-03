e7a7f20a5dcd70fd93d81dd0441fe3403704ff08
sem0ark <arcadii.sem@gmail.com>
2024-05-03 21:01:37 +0200

    refactor: add PInteger tests

--- tests/framework/property.spec.ts
+++ tests/framework/property.spec.ts
@@ -1,5 +1,8 @@
 import { disableLogging } from '../../src/framework/utilities/logging';
-import { Property } from '../../src/framework/reactive_properties/property';
+import {
+  PInteger,
+  Property,
+} from '../../src/framework/reactive_properties/property';
 
 describe('Test functionality of elementary property', () => {
   beforeEach(() => {
@@ -36,6 +39,91 @@ describe('Test functionality of elementary property', () => {
     prop.onChange(cb);
 
     expect(prop.get()).toBe('new');
+    expect(cb.mock.calls).toStrictEqual([]);
+  });
+});
+
+describe('Test functionality of PInteger', () => {
+  beforeEach(() => {
+    disableLogging();
+  });
+
+  it('must be able set value', () => {
+    const prop = new PInteger('test property', 0);
+    expect(prop.get()).toBe(0);
+  });
+
+  it('must update set value', () => {
+    const prop = new PInteger('test property', 0);
+    prop.set(10);
+    expect(prop.get()).toBe(10);
+  });
+
+  it('must update set value on inc', () => {
+    const prop = new PInteger('test property', 0);
+    prop.inc();
+    expect(prop.get()).toBe(1);
+  });
+
+  it('must update set value on dec', () => {
+    const prop = new PInteger('test property', 0);
+    prop.dec();
+    expect(prop.get()).toBe(-1);
+  });
+
+  it('must update set value on inc with specified step', () => {
+    const prop = new PInteger('test property', 0);
+    prop.inc(2);
+    expect(prop.get()).toBe(2);
+  });
+
+  it('must update set value on dec with specified step', () => {
+    const prop = new PInteger('test property', 0);
+    prop.dec(2);
+    expect(prop.get()).toBe(-2);
+  });
+
+  it('must call listeners on value update', () => {
+    const cb = jest.fn();
+
+    const prop = new PInteger('test property', 0);
+    prop.onChange(cb);
+    prop.inc();
+
+    expect(prop.get()).toBe(1);
+    expect(cb.mock.calls).toStrictEqual([[1, 0, prop]]);
+  });
+
+  it('must not call listeners after value update', () => {
+    const cb = jest.fn();
+
+    const prop = new PInteger('test property', 0);
+    prop.inc();
+    prop.onChange(cb);
+
+    expect(prop.get()).toBe(1);
+    expect(cb.mock.calls).toStrictEqual([]);
+  });
+
+  it('must call listeners on value update dec', () => {
+    const cb = jest.fn();
+
+    const prop = new PInteger('test property', 0);
+    prop.onChange(cb);
+    prop.dec();
+
+    expect(prop.get()).toBe(1);
+    expect(cb.mock.calls).toStrictEqual([[1, 0, prop]]);
+  });
+
+  it('must not call listeners after value update dec', () => {
+    const cb = jest.fn();
+
+    const prop = new PInteger('test property', 0);
+    prop.dec();
+    prop.onChange(cb);
+
+    expect(prop.get()).toBe(1);
     expect(cb.mock.calls).toStrictEqual([]);
   });
 });
