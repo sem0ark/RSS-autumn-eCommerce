44505c2c90434bb36b3f7c8a57610168e70afd00
sem0ark <arcadii.sem@gmail.com>
2024-05-06 18:14:16 +0200

    docs: add comments to the HTML Component methods

--- src/framework/ui_components/htmlComponent.ts
+++ src/framework/ui_components/htmlComponent.ts
@@ -136,6 +136,14 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   *
+   * @param name name of HTMLElement event
+   * @param handler callback handling the received Event
+   * @param preventDefault = true Optional, add preventDefault() call to the addEventListener method
+   * @param stopPropagation = true Optional, add stopPropagation() to the addEventListener method
+   * @returns
+   */
   public on(
     name: string,
     handler: (e: Event) => void,
@@ -157,6 +165,13 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   * Add on click event listener
+   * @param handler
+   * @param preventDefault
+   * @param stopPropagation
+   * @returns
+   */
   public onClick(
     handler: (e: Event) => void,
     preventDefault = false,
@@ -166,6 +181,13 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   * Add on contextmenu event listener
+   * @param handler
+   * @param preventDefault
+   * @param stopPropagation
+   * @returns
+   */
   public onRClick(
     handler: (e: Event) => void,
     preventDefault = false,
@@ -175,6 +197,13 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   * Add on input event listener
+   * @param handler
+   * @param preventDefault
+   * @param stopPropagation
+   * @returns
+   */
   public onInput(
     handler: (e: InputEvent) => void,
     preventDefault = false,
@@ -189,6 +218,14 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   * Utility function, which will addEventListener on input event and call callback function with the actual full text value being entered
+   *
+   * @param handler `(string) => void` handling text input
+   * @param preventDefault
+   * @param stopPropagation
+   * @returns
+   */
   public onInputValue(
     handler: (data: string) => void,
     preventDefault = false,
@@ -203,6 +240,13 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   * Add on change event listener
+   * @param handler
+   * @param preventDefault
+   * @param stopPropagation
+   * @returns
+   */
   public onChange(
     handler: (e: InputEvent) => void,
     preventDefault = false,
@@ -217,6 +261,13 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   * Add on submit event listener
+   * @param handler
+   * @param preventDefault
+   * @param stopPropagation
+   * @returns
+   */
   public onSubmit(
     handler: (e: SubmitEvent) => void,
     preventDefault = false,
@@ -231,6 +282,13 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   * Add on keydown event listener
+   * @param handler
+   * @param preventDefault
+   * @param stopPropagation
+   * @returns
+   */
   public onKeypress(
     handler: (key: string) => void,
     preventDefault = false,
@@ -245,36 +303,73 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   * Add sub-components to the current HTML component
+   * @param children Children components to be added to the HTML Component
+   * @returns
+   */
   public add(...children: Component[]) {
     this._children.push(...children);
     return this;
   }
 
+  /**
+   * Sequentially (in the same order as the methods were called) add text entry inside the HTML component, will render it as an added TextNode.
+   * @param txt Some text data
+   * @returns
+   */
   public text(txt: string) {
     this._children.push(new TextComponent(txt));
     return this;
   }
 
+  /**
+   * Add classes to the component's `classList`
+   * @param classes List of classes to add to the HTML Component's classList on render.
+   * @returns
+   */
   public cls(...classes: string[]) {
     this._classList.push(...classes);
     return this;
   }
 
+  /**
+   * Set a new tag value for the component.
+   * @param tag
+   * @returns
+   */
   public tag(tag: string) {
     this._tag = tag;
     return this;
   }
 
+  /**
+   * Add HTMLElement ID to the component. Set an `id=` value for the HTML component.
+   * @param id
+   * @returns
+   */
   public id(id: string) {
     this._id = id;
     return this;
   }
 
+  /**
+   * Set attribute value to the component
+   * @param name name of the attribute
+   * @param value string value of the attribute
+   * @returns
+   */
   public attr(name: string, value: string = '') {
     this._attributes[name] = value;
     return this;
   }
 
+  /**
+   * Add inline styles to the component
+   * @param name style type name
+   * @param value style text value
+   * @returns
+   */
   public style(name: string, value: string = '') {
     this._styles[name] = value;
     return this;
@@ -329,6 +424,12 @@ export class HTMLComponent extends Component {
   // All Apply methods are used after the element was created already
   // it is useful because we won't need to use ID selection through DOM
 
+  /**
+   * Add a listener to the specified property to apply some callback to the rendered HTMLElement on any change of the property.
+   * @param prop
+   * @param resolver `(node: HTMLElement, newValue: T) => void`
+   * @returns
+   */
   public propNode<T extends PropertyValueType>(
     prop: Property<T>,
     resolver: (node: HTMLElement, newValue: T) => void
@@ -340,22 +441,43 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   * Set style value to the already rendered HTML Element
+   * @param name
+   * @param value
+   * @returns
+   */
   public applyStyle(name: string, value: string = '') {
     if (this._node) this._node.style.setProperty(name, value);
     return this;
   }
 
+  /**
+   * Set attribute value to the already rendered HTML Element
+   * @param name
+   * @param value
+   * @returns
+   */
   public applyAttr(name: string, value: string = '') {
     if (this._node) this._node.setAttribute(name, value);
     return this;
   }
 
+  /**
+   * Apply style value to the already rendered HTML Element
+   * @returns Rendered HTML Element Node if rendered, else null
+   */
   public getNode() {
     return this._node;
   }
 
   private renderListeners: ((n: HTMLElement) => void)[] = [];
 
+  /**
+   * Apply a callback to the HTML Element, when component gets rendered.
+   * @param handler Callback applied to the rendered HTMLElement, when it gets rendered.
+   * @returns
+   */
   public onRender(handler: (n: HTMLElement) => void) {
     this.renderListeners.push(handler);
     return this;
@@ -374,6 +496,10 @@ export class HTMLComponent extends Component {
     );
   }
 
+  /**
+   * Apply some actions, when the rendered component changes its visibly in the viewport.
+   * @param handler
+   */
   public onVisibilityChange(handler: (visible: boolean) => void) {
     if (!this.pVisible) {
       this.pVisible = new PBoolean((this._id || '') + 'pVisible', false);
@@ -398,6 +524,10 @@ export class HTMLComponent extends Component {
     this.pVisible.onChange(handler);
   }
 
+  /**
+   * Apply some actions, when the rendered component becomes visible in the viewport.
+   * @param handler
+   */
   public onAppear(handler: () => void) {
     this.onVisibilityChange((visible) => {
       if (visible) handler();
@@ -405,6 +535,10 @@ export class HTMLComponent extends Component {
     return this;
   }
 
+  /**
+   * Apply some actions, when the rendered component disappears from the viewport (no longer visible).
+   * @param handler
+   */
   public onDisappear(handler: () => void) {
     this.onVisibilityChange((visible) => {
       if (!visible) handler();
