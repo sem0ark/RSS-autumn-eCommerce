96dd38258ab851675e81f85b1ab288ad40349aec
Larisa Novozhilova <kara4516@gmail.com>
2024-05-07 19:52:53 +0300

    refactor: independent main component added

--- src/view/login/loginPage.ts
+++ src/view/login/loginPage.ts
@@ -3,15 +3,15 @@ import { Page } from '../../framework/ui_components/page';
 
 import { containerComponents } from '../shared/containerComponents';
 import { navContainer } from '../shared/nav/nav';
+import { mainComponents } from '../shared/main/main';
 import { footerComponents } from '../shared/footer/footer';
-import { htmlComponents } from '../shared/htmlComponents';
 
 const { wrapper } = containerComponents;
 const { navElement } = navContainer;
-const { main } = htmlComponents;
+const { mainContainer } = mainComponents;
 const { footerContainer } = footerComponents;
 
 export const loginPage = () =>
   new Page(() => {
-    return wrapper(navElement(), main('main').cls('main'), footerContainer());
+    return wrapper(navElement(), mainContainer(), footerContainer());
   });
