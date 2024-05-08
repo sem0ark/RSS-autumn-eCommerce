7ee2a837745963afd6598f7dadcbe9b074a77099
Larisa Novozhilova <kara4516@gmail.com>
2024-05-08 20:26:58 +0300

    feat: add UI form for login

--- src/view/login/mainLoginPage.ts
+++ src/view/login/mainLoginPage.ts
@@ -2,8 +2,10 @@ import { htmlComponents } from '../shared/htmlComponents';
 import { Component } from '../../framework/ui_components/component';
 import { PropertyValueType } from '../../framework/reactive_properties/types';
 import background from '../../../assets/background.jpg';
+import { formComponents } from './form';
 
 const { main, div, img } = htmlComponents;
+const { formContainer } = formComponents;
 
 type CC = (Component | PropertyValueType)[];
 
@@ -15,7 +17,9 @@ const mainContainer = (...children: CC) =>
         .cls('background-logo-img-container')
         .add(img(background).cls('background-logo-img'))
     )
-    .add(div().cls('main-logo-page-container', '_container'));
+    .add(
+      div().cls('main-logo-page-container', '_container').add(formContainer())
+    );
 
 export const mainComponents = {
   mainContainer,
