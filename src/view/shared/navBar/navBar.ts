37552531b98a035ac88e82e41e6293111d8e8575
sem0ark <arcadii.sem@gmail.com>
2024-05-12 17:00:10 +0200

    feat: update navbar

--- src/view/shared/navBar/navBar.ts
+++ src/view/shared/navBar/navBar.ts
@@ -6,32 +6,39 @@ import logoSVG from '../../../../assets/logo-site.svg';
 import { htmlComponents } from '../htmlComponents';
 import { inputComponents } from '../inputComponents';
 import { containerComponents } from '../containerComponents';
+import { factories } from '../../../framework/factories';
 
 const { div, nav, ul, li, link, iconSvg } = htmlComponents;
 const { buttonSecondary, buttonPrimary, buttonIcon } = inputComponents;
 const { containerMaxWidth, containerFlexRow } = containerComponents;
 
-export const navBar = () =>
-  containerMaxWidth
+export const navBar = () => {
+  const menuOpen = factories.pboolean(false, 'menuOpen');
+
+  return containerMaxWidth
     .cls('navbar')(
-      div(iconSvg(logoSVG)).cls('logo-site'),
+      div(iconSvg(logoSVG))
+        .cls('logo-site')
+        .onClick(() => menuOpen.toggle()),
       div(
         nav(
           ul(
-            li(link('/', 'Home').cls('header-link', 'active-link')).cls(
-              'header-item'
-            ),
-            li(link('/catalog', 'Catalog product').cls('header-link')).cls(
-              'header-item'
-            ),
-            li(link('/about', 'About us').cls('header-link')).cls('header-item')
+            li(link('/', 'Home').cls('header-link', 'active-link')),
+            li(link('/catalog', 'Catalog').cls('header-link')),
+            li(link('/about', 'About us').cls('header-link'))
           ).cls('header-list')
         ),
         containerFlexRow({ gap: 20 })(
           buttonSecondary('Sign up'),
           buttonPrimary('Login')
-        )
+        ).cls('navbar-buttons')
       ).cls('header-nav-container'),
-      containerFlexRow({ gap: 20 })(buttonIcon(userSVG), buttonIcon(cartSVG))
+
+      containerFlexRow({ gap: 20 })(
+        buttonIcon(userSVG),
+        buttonIcon(cartSVG)
+      ).cls('navbar-buttons')
     )
+    .propClass(menuOpen, (o) => (o ? ['navbar-menu-open'] : []))
     .tag('header');
+};
