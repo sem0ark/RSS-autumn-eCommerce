f8b3a0f582e7b89f6e787d15ca442c9afd53bc20
sem0ark <arcadii.sem@gmail.com>
2024-05-17 11:46:05 +0200

    feat: update structure of auth context

--- src/contexts/authContext.ts
+++ src/contexts/authContext.ts
@@ -38,16 +38,18 @@ class AuthContext {
 
     if (!authConnector.isLoggedIn()) {
       debug('!authConnector.isLoggedIn()');
-      this.userData.set(null);
+      this.attemptLogout();
     }
 
     if (this.userIsLoggedIn.get() || authConnector.isLoggedIn()) {
       debug('this.userIsLoggedIn.get() || authConnector.isLoggedIn()');
       authConnector.runReSignInWorkflow().catch(() => {
         notificationContext.addError('Session expired');
-        this.userData.set(null);
+        this.attemptLogout();
         return Promise.resolve();
       });
+    } else {
+      this.attemptLogout();
     }
   }
 
