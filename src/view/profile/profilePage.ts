6ef57729620ac0a15f69fb3fe7e9d619301418a9
sem0ark <arcadii.sem@gmail.com>
2024-05-17 15:18:37 +0200

    feat: add sidebar layout

--- src/view/profile/profilePage.ts
+++ src/view/profile/profilePage.ts
@@ -1,11 +1,33 @@
 import './profilePage.css';
+import signOutSVG from '../../../assets/sign-out.svg';
+import profileSVG from '../../../assets/icon-user-profile.svg';
 
 import { Page } from '../../framework/ui_components/page';
 import { sidebarLayout } from '../shared/layouts/sidebarLayout';
 import { htmlComponents } from '../shared/htmlComponents';
+import { textComponents } from '../shared/textComponents';
+import { factories } from '../../framework/factories';
+import { authContext } from '../../contexts/authContext';
 
-const { aside, main } = htmlComponents;
+const { functional } = factories;
+const { aside, main, div } = htmlComponents;
+const { header2Orange, header3Orange, textSubtext, textMenuEntry } =
+  textComponents;
 
 export const profilePage = new Page(() => {
-  return sidebarLayout('Create an account')(aside(), main());
+  return sidebarLayout('My Info')(
+    aside(
+      functional(() => header2Orange(`Hello, ${authContext.userName.get()}`)),
+      textSubtext('Welcome to your account'),
+      div(
+        textMenuEntry(profileSVG, 'My Info').cls('active'),
+        textMenuEntry(signOutSVG, 'Hello')
+      ).cls('menu')
+    ).cls('profile-page'),
+    main(
+      header2Orange('My Info'),
+      header3Orange('Contact Details'),
+      header3Orange('Address')
+    ).cls('profile-page')
+  );
 }, 'Profile | True Colors');
--- src/view/shared/textComponents.ts
+++ src/view/shared/textComponents.ts
@@ -1,16 +1,24 @@
+import './textComponents.css';
+
 import { CC } from '../../framework/ui_components/htmlComponent';
 import { htmlComponents } from './htmlComponents';
 
-const { h2, h3, p, span } = htmlComponents;
+const { h2, h3, p, span, div, iconSvg } = htmlComponents;
 
 const header2Orange = (...c: CC) =>
   h2(span(), ...c).cls('text-components-header');
+
 const header3Orange = (...c: CC) =>
   h3(span(), ...c).cls('text-components-header');
+
 const textSubtext = p.cls('text-components-subtext');
+
+const textMenuEntry = (iconPath: string, ...c: CC) =>
+  div.cls('text-components-menu-entry')(iconSvg(iconPath), ...c);
 
 export const textComponents = {
   header2Orange,
   header3Orange,
   textSubtext,
+  textMenuEntry,
 };
