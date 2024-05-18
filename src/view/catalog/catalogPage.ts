a8f8de79e09771afd1b8237151aac82f15386474
sem0ark <arcadii.sem@gmail.com>
2024-05-18 20:58:20 +0200

    feat: merge changes

---
+++ src/view/catalog/catalogPage.ts
@@ -0,0 +1,8 @@
+import { Page } from '../../framework/ui_components/page';
+import { htmlComponents } from '../shared/htmlComponents';
+
+const {} = htmlComponents;
+
+export const catalogPage = new Page(() => {
+  return;
+}, 'Catalog | True Colors');
